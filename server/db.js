35ae4c26eeb333a1becf3907e755434a16961862
Emilio Cassaro <s319083@studenti.polito.it>
2023-10-22 10:42:30 +0200

    DB.js modification in order to test the DB

--- server/db.js
+++ server/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2/promise');
+/*const mysql = require('mysql2/promise');
 const config = require('./config');
 
 async function query(connection, sql, params) {
@@ -9,4 +9,36 @@ async function query(connection, sql, params) {
 
 module.exports = {
   query
-}
+}*/
+
+
+const mysql = require('mysql2');
+const config = require('./config');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', 
+  //password: 'root', 
+  database: 'db_se2_2023_team3' 
+});
+
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error during the connection!!:', err);
+    return;
+  }
+  console.log('Connected!!.');
+
+  
+  connection.query('SELECT * FROM service WHERE ServiceID=1', (error, results, fields) => {
+    if (error) {
+      console.error('Error in query:', error);
+      return;
+    }
+    console.log('Results:', results);
+
+ 
+    connection.end();
+  });
+});
